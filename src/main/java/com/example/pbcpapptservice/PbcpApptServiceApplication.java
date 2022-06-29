607bfb6f33b1900c51c0600ce87ff8a96db11be6
tylerbarton <tylervbarton@gmail.com>
2022-06-29 12:16:15 -0500

    Initial commit

---
+++ src/main/java/com/example/pbcpapptservice/PbcpApptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pbcpapptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbcpApptServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbcpApptServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pbcpapptservice/PbcpApptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pbcpapptservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbcpApptServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
