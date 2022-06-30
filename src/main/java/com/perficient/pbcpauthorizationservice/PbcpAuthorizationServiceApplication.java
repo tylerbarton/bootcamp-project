68638372e16ab13cfa1e7b6afe40057e758305ce
tylerbarton <tylervbarton@gmail.com>
2022-06-30 13:14:21 -0500

    Initial commit

---
+++ src/main/java/com/perficient/pbcpauthorizationservice/PbcpAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.pbcpauthorizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbcpAuthorizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbcpAuthorizationServiceApplication.class, args);
+    }
+
+}
