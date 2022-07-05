8f91962fae0dc922cc5f031188996496253c9d53
tylerbarton <tylervbarton@gmail.com>
2022-07-05 13:01:10 -0500

    Add repository annotation to repository

--- src/main/java/com/perficient/pbcpapptservice/repository/ApptRepository.java
+++ src/main/java/com/perficient/pbcpapptservice/repository/ApptRepository.java
@@ -4,6 +4,7 @@ import com.perficient.pbcpapptservice.domain.Appointment;
 import com.perficient.pbcpapptservice.domain.ApptStatus;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,7 +14,7 @@ import java.util.Optional;
  * @version 1.0, 6/29/2022
  * @project PBCP-ApptService
  */
-@Document(collection = "appointments")
+@Repository
 public interface ApptRepository extends MongoRepository<Appointment, Long> {
     Optional<Appointment> findAppointmentByNameAndDeletedIsFalse(String name);
     boolean existsAppointmentByIdAndAndDeletedIsFalse(Long id);
