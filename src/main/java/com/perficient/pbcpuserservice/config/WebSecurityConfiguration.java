c0478391bbce5bcdb08a878d8019643e8bb3a84a
tylerbarton <tylervbarton@gmail.com>
2022-07-12 14:29:51 -0500

    Replace http authorize requests with oauth2Login

--- src/main/java/com/perficient/pbcpuserservice/config/WebSecurityConfiguration.java
+++ src/main/java/com/perficient/pbcpuserservice/config/WebSecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.perficient.pbcpuserservice.config;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,17 +15,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {// @formatter:off
-        http.cors()
+        http.authorizeRequests().anyRequest().authenticated()
                 .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
-                .hasAuthority("SCOPE_read")
-                .antMatchers(HttpMethod.POST, "/api/foos")
-                .hasAuthority("SCOPE_write")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+                .oauth2Login();// @formatter:on
+
+
+//        http.cors()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
+//                .hasAuthority("SCOPE_read")
+//                .antMatchers(HttpMethod.POST, "/api/foos")
+//                .hasAuthority("SCOPE_write")
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
     }//@formatter:on
 }
