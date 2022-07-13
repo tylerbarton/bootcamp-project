851194c6136e231680f595cbd6712470c081591d
tylerbarton <tylervbarton@gmail.com>
2022-07-13 12:56:46 -0500

    Disable authentication requirement in MockMvc for unit testing

--- src/backend/user/src/main/java/com/perficient/pbcpuserservice/config/WebSecurityConfiguration.java
+++ src/backend/user/src/main/java/com/perficient/pbcpuserservice/config/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.perficient.pbcpuserservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 @EnableWebSecurity
+@Profile(value = {"development", "production"})
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     // Gets the values from the application.properties file.
